f9e1b35d198d097d9b0612560a5aa128bc005c91
s-vasiliev <vasyliev@mynfo.comvasyliev@mynfo.comvasyliev@mynfo.com>
2016-09-16 17:07:27 +0300

    33

--- src/test/java/Tests2.java
+++ src/test/java/Tests2.java
@@ -9,6 +9,6 @@ public class Tests2 {
             GradleTest.Calc calc = new GradleTest.Calc();
             int summand1 = 3;
             int summand2 = 5;
-            assertEquals("Sum must be " + (summand1 + summand2), (summand1 + summand2 + 8 - 8), calc.Add(summand1, summand2));
+            assertEquals("Sum must be " + (summand1 + summand2), (summand1 + summand2 + 8 - 9), calc.Add(summand1, summand2));
         }
 }
