340f63587373004158a7a58069c56a00649deaf6
s-vasiliev <vasyliev@mynfo.comvasyliev@mynfo.comvasyliev@mynfo.com>
2017-01-13 19:06:06 -0800

    rr

--- src/test/java/Tests2.java
+++ src/test/java/Tests2.java
@@ -17,6 +17,6 @@ public class Tests2 {
         GradleTest.Calc calc = new GradleTest.Calc();
         int summand1 = 3;
         int summand2 = 6;
-        assertEquals("Sum must be " + (summand1 + summand2), (summand1 + summand2 + 44 - 44), calc.Add(summand1, summand2));
+        assertEquals("Sum must be " + (summand1 + summand2), (summand1 + summand2 + 55 - 55), calc.Add(summand1, summand2));
     }
 }
